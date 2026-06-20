4a653c50a88594394986080980e244555a4d0b5f
Sreeeramya <160019764+Sreeeramya@users.noreply.github.com>
2026-06-20 15:58:13 +0530

    Time: 6 ms (5.76%), Space: 45.8 MB (5.37%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-        int j=0;
         int ans[]=new int[prices.length];
         Stack<Integer> s1=new Stack<>();
         int n=prices.length;
@@ -11,27 +10,18 @@ class Solution {
                     s1.pop();
                 }
                 if(!s1.isEmpty() && s1.peek()<=prices[i]){
-                    ans[j]=prices[i]-s1.peek();
+                    ans[i]=prices[i]-s1.peek();
                     s1.push(prices[i]);
                     i--;
-                    j++;
+                    
                 }
             }
             if(s1.isEmpty() || i==n-1 || i==n-2){
-                ans[j]=prices[i];
+                ans[i]=prices[i];
                 s1.push(prices[i]);
                 i--;
-                j++;
+                
             }
-        }
-        int a=0;
-        int b=ans.length-1;
-        while(a<=b){
-            int temp=ans[a];
-            ans[a]=ans[b];
-            ans[b]=temp;
-            a++;
-            b--;
         }
         return ans;
     }
