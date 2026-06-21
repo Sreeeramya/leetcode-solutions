f1bbc410606d453c6f4ec2e656de970727c20257
Sreeeramya <160019764+Sreeeramya@users.noreply.github.com>
2026-06-21 15:19:29 +0530

    Time: 32 ms (16.53%), Space: 48.6 MB (83.1%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,5 +1,8 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
+        if(matrix.length==0){
+            return 0;
+        }
         int m=matrix.length;
         int n=matrix[0].length;
         int v[]=new int[n];
