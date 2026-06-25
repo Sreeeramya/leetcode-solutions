10637ab27fed0271237649a39b0cf144df87d9bb
Sreeeramya <160019764+Sreeeramya@users.noreply.github.com>
2026-06-25 11:37:41 +0530

    Time: 3 ms (62.64%), Space: 43.6 MB (50.13%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String makeGood(String s) {
+        Stack<Character> s1=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(!s1.isEmpty() && Math.abs(s1.peek()-ch)==32){
+                s1.pop();
+            }
+            else{
+                s1.push(ch);
+            }
+        }
+        if(s1.size()>0){
+        StringBuilder str=new StringBuilder();
+        for(char c:s1){
+            str.append(c);
+        }
+        return str.toString();
+        }
+        return "";
+    }
+}
