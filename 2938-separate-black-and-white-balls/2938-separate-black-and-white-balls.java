bc7471ec5de9b382f9c52a9cd838eb47d65777b3
Sreeeramya <160019764+Sreeeramya@users.noreply.github.com>
2026-06-29 00:01:11 +0530

    Time: 7 ms (100%), Space: 47.5 MB (85.02%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long minimumSteps(String s) {
+        long ans=0;
+        long ones=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='1'){
+                ones++;
+            }
+            else{
+                ans+=ones;
+            }
+        }
+        return ans;
+    }
+}
