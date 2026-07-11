bcf2f0991c5cd3feea803891f390edde87c1e732
Sreeeramya <160019764+Sreeeramya@users.noreply.github.com>
2026-07-11 18:24:41 +0530

    Time: 17 ms (20.59%), Space: 47.2 MB (86.89%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
-        int ans=0;
+        
         HashMap<Integer,Integer> h1=new HashMap<>();
         for(int a:nums){
             h1.put(a,h1.getOrDefault(a,0)+1);
@@ -8,9 +8,9 @@ class Solution {
         int n=nums.length/2;
         for(int a:h1.keySet()){
             if(h1.get(a)==n){
-                ans=a;
+                return a;
             }
         }
-        return ans;
+        return -1;
     }
 }
