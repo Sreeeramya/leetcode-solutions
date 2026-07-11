cd366c34cfd59d8025fc4c75d0440c6e8011ed8a
Sreeeramya <160019764+Sreeeramya@users.noreply.github.com>
2026-07-11 16:34:46 +0530

    Time: 6 ms (37.2%), Space: 45.2 MB (53.82%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
@@ -1,15 +1,13 @@
 class Solution {
     public int findLucky(int[] arr) {
         int ans=-1;
-        int max=Integer.MIN_VALUE;;
         HashMap<Integer,Integer> h1=new HashMap<>();
         for(int a:arr){
             h1.put(a,h1.getOrDefault(a,0)+1);
         }
         for(int a:h1.keySet()){
-            if(h1.get(a)==a && h1.get(a)>=max ){
-                max=Math.max(max,h1.get(a));
-                ans=a;
+            if(h1.get(a)==a){
+                ans=Math.max(ans,a);
             }
         }
         
