f97c2e63f77aed8f61d142481e3dd5646ef8a89b
Sreeeramya <160019764+Sreeeramya@users.noreply.github.com>
2026-07-11 17:17:16 +0530

    Time: 4 ms (29.92%), Space: 46.5 MB (21.04%) - LeetHub

---
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int mostFrequent(int[] nums, int key) {
+        int max=Integer.MIN_VALUE;
+        HashMap<Integer,Integer> h1=new HashMap<>();
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==key){
+                h1.put(nums[i+1],h1.getOrDefault(nums[i+1],0)+1);
+            }
+        }
+        for(int a:h1.keySet()){
+            if(h1.get(a)>max){
+                max=Math.max(h1.get(a),max);
+            }
+        }
+        for(int a:h1.keySet()){
+            if(h1.get(a)==max){
+                return a;
+            }
+        }
+        return 0;
+    }
+}
