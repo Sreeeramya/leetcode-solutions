f66492f408aa4d3f2998d21a713873a0c8efc6a7
Sreeeramya <160019764+Sreeeramya@users.noreply.github.com>
2026-07-25 21:55:11 +0530

    Time: 0 ms (100%), Space: 44.3 MB (57.77%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -9,40 +9,29 @@
  * }
  */
 class Solution {
-    public ListNode insertLast(ListNode head,int value){
-        ListNode n1=new ListNode(value);
-        if(head==null){
-            return n1;
-        }
-        ListNode temp=head;
-        while(temp.next!=null){
-            temp=temp.next;
-        }
-        temp.next=n1;
-        return head;
-    }
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode f=list1;
-        ListNode s=list2;
-        ListNode ans=null;
-        while(f!=null && s!=null){
-            if(f.val<s.val){
-                ans=insertLast(ans,f.val);
-                f=f.next;
+        ListNode i=list1;
+        ListNode j=list2;
+        ListNode d=new ListNode(-1);
+        ListNode k=d;
+        while(i!=null && j!=null){
+            if(i.val<j.val){
+                k.next=i;
+                k=i;
+                i=i.next;
             }
             else{
-                ans=insertLast(ans,s.val);
-                s=s.next;
+                k.next=j;
+                k=j;
+                j=j.next;
             }
         }
-        while(f!=null){
-            ans=insertLast(ans,f.val);
-            f=f.next;
+        if(i==null){
+            k.next=j;
         }
-        while(s!=null){
-            ans=insertLast(ans,s.val);
-            s=s.next;
+        else if(j==null){
+            k.next=i;
         }
-        return ans;
+        return d.next;
     }
 }
