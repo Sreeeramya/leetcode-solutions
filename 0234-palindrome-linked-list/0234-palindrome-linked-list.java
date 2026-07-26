bad4a265d82c404e843a9b5033e2f01710ccda20
Sreeeramya <160019764+Sreeeramya@users.noreply.github.com>
2026-07-26 17:19:49 +0530

    Time: 4 ms (66.02%), Space: 94.3 MB (80.09%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,23 +9,56 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        ArrayList<Integer> a1=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null){
-            a1.add(temp.val);
-            temp=temp.next;
+    public ListNode reverse(ListNode head){
+        ListNode c=head;
+        ListNode p=null;
+        ListNode f=null;
+        while(c!=null){
+            f=c.next;
+            c.next=p;
+            p=c;
+            c=f;
         }
-        int n=a1.size();
-        int i=0;
-        int j=n-1;
-        while(i<j){
-            if(a1.get(i)!=a1.get(j)){
+        return p;
+    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode s=head;
+        ListNode f=head;
+        while(f.next!=null && f.next.next!=null){
+            s=s.next;
+            f=f.next.next;
+        }
+        ListNode h2=s.next;
+        s.next=null;
+        h2=reverse(h2);
+        ListNode i=head;
+        ListNode j=h2;
+        while(j!=null){
+            if(i.val!=j.val){
                 return false;
             }
-            i++;
-            j--;
+            i=i.next;
+            j=j.next;
         }
         return true;
     }
+    // public boolean isPalindrome(ListNode head) {
+    //     ArrayList<Integer> a1=new ArrayList<>();
+    //     ListNode temp=head;
+    //     while(temp!=null){
+    //         a1.add(temp.val);
+    //         temp=temp.next;
+    //     }
+    //     int n=a1.size();
+    //     int i=0;
+    //     int j=n-1;
+    //     while(i<j){
+    //         if(a1.get(i)!=a1.get(j)){
+    //             return false;
+    //         }
+    //         i++;
+    //         j--;
+    //     }
+    //     return true;
+    // }
 }
