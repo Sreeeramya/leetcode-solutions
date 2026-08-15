9e860e75001363f522fe2a86574f14fcf0f73ea4
Sreeeramya <160019764+Sreeeramya@users.noreply.github.com>
2026-08-15 07:51:12 +0530

    Time: 1 ms (99.9%), Space: 45.7 MB (19.17%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int compress(char[] chars) {
+        if(chars.length==1){
+            return 1;
+        }
+        StringBuilder str=new StringBuilder();
+        int i=0;
+        int j=0;
+        int k=0;
+        while(j<chars.length){
+            int c=0;
+            while(j<chars.length && chars[i]==chars[j]){
+                c++;
+                j++;
+            }
+            chars[k++]=chars[i];
+            if(c>1){
+                String b=String.valueOf(c);
+                for(char ch:b.toCharArray()){
+                    chars[k++]=ch;
+                }
+            }
+            i+=c;
+        }
+        return k;
+    }
+}
