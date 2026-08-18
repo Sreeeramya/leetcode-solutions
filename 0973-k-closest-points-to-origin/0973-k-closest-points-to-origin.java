e62f5cc053861a420e102b9b010c28dd6d1295b2
Sreeeramya <160019764+Sreeeramya@users.noreply.github.com>
2026-08-18 18:15:51 +0530

    Time: 31 ms (48.59%), Space: 53.7 MB (72.09%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,24 +1,18 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> p=new PriorityQueue<>(
-            (a,b)->{
-                int distA=a[0]*a[0]+a[1]*a[1];
-                int distB=b[0]*b[0]+b[1]*b[1];
-                return distB-distA;
-            }
-        );
-        for(int p1[]:points){
-            p.offer(p1);
+        PriorityQueue<int[]> p=new PriorityQueue<>((a,b)->Integer.compare(b[0]*b[0]+b[1]*b[1],a[0]*a[0]+a[1]*a[1]));
+        for(int a[]:points){
+            p.add(a);
             if(p.size()>k){
-                p.poll();
+                p.remove();
             }
         }
-        int ans[][]=new int[k][2];
-        for(int i=0;i<k;i++){
-            int a[]=p.poll();
-            ans[i][0]=a[0];
-            ans[i][1]=a[1];
+        int arr[][]=new int[k][2];
+        int i=0;
+        while(p.size()>0){
+            int arr1[]=p.remove();
+            arr[i++]=arr1;
         }
-        return ans;
+        return arr;
     }
 }
