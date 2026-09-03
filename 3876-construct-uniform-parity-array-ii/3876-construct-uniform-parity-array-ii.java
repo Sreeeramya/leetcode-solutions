3ffba2b0aa61ef7d457778c1033328496fcfeb0a
Sreeeramya <160019764+Sreeeramya@users.noreply.github.com>
2026-09-03 12:23:44 +0530

    Time: 2 ms (100%), Space: 121.6 MB (62.32%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int min=nums1[0];
+        int m_idx=0;
+        for(int i=1;i<nums1.length;i++){
+            if(nums1[i]<min){
+                min=nums1[i];
+                m_idx=i;
+            }
+        }
+        if(min%2!=0){
+            return true;
+        }
+        if(min%2==0){
+            for(int i=0;i<nums1.length;i++){
+                if(nums1[i]%2!=0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
