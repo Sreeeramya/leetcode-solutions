d62d9c8a5f61705310e8a976630f82fa6f8bb3e1
Sreeeramya <160019764+Sreeeramya@users.noreply.github.com>
2026-09-18 21:28:09 +0530

    Time: 1 ms (16.38%), Space: 46.7 MB (5.32%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,23 +13,44 @@
  *     }
  * }
  */
+ class Pair{
+    long max;
+    long min;
+    Pair(long max,long min){
+        this.max=max;
+        this.min=min;
+    }
+ }
 class Solution {
+    boolean flag;
     public boolean isValidBST(TreeNode root) {
-        boolean val=true;
-        ArrayList<Integer> l1=new ArrayList<>();
-        find(root,l1);
-        for(int i=1;i<l1.size();i++){
-            if(l1.get(i)<=l1.get(i-1)){
-                val=false;
-                break;
-            }
-        }
-        return val;
+        // boolean val=true;
+        // ArrayList<Integer> l1=new ArrayList<>();
+        // find(root,l1);
+        // for(int i=1;i<l1.size();i++){
+        //     if(l1.get(i)<=l1.get(i-1)){
+        //         val=false;
+        //         break;
+        //     }
+        // }
+        // return val;
+        flag=true;
+        maxmin(root);
+        return flag;
     }
-    public void find(TreeNode root,ArrayList<Integer> l1){
-        if(root==null)return;
-        find(root.left,l1);
-        l1.add(root.val);
-        find(root.right,l1);
+    public Pair maxmin(TreeNode root){
+        if(root==null)return new Pair(Long.MIN_VALUE,Long.MAX_VALUE);
+        Pair lst=maxmin(root.left);
+        Pair rst=maxmin(root.right);
+        long mx=Math.max(root.val,Math.max(lst.max,rst.max));
+        long mn=Math.min(root.val,Math.min(lst.min,rst.min));
+        if(lst.max>=root.val || rst.min<=root.val)flag=false;
+        return new Pair(mx,mn);
     }
+    // public void find(TreeNode root,ArrayList<Integer> l1){
+    //     if(root==null)return;
+    //     find(root.left,l1);
+    //     l1.add(root.val);
+    //     find(root.right,l1);
+    // }
 }
