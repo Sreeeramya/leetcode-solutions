953953d8cd896a1b897dc3585f1c99919f8dc24e
Sreeeramya <160019764+Sreeeramya@users.noreply.github.com>
2026-09-22 08:32:52 +0530

    Time: 0 ms (100%), Space: 42.2 MB (47.37%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,7 +1,23 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int [][]dp=new int[m+1][n+1];
-        return find(m,n,dp);
+        // int [][]dp=new int[m+1][n+1];
+        // return find(m,n,dp);
+        int [][]dp=new int[m][n];
+        return tab(m,n,dp);
+    }
+    public int tab(int m,int n,int dp[][]){
+        for(int i=0;i<n;i++){
+            dp[0][i]=1;
+        }
+        for(int i=0;i<m;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i][j-1]+dp[i-1][j];
+            }
+        }
+        return dp[m-1][n-1];
     }
     public int find(int m,int n,int dp[][]){
         if(m==1 || n==1)return 1;
